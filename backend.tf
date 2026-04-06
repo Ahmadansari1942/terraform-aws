13476cda982b52c7828e2f10bc4b54697ca805aa
Ahmadansari1942 <sn0259152@gmail.com>
2026-04-06 15:44:44 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,5 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-ahmadhdfdghfksjdgds"
     key    = "vpc-project/terraform.tfstate"
-    region = "eu-north-1"   # 👈 yahan change kar
-  }
+    region = "eu-north-1"   # 👈 
 }
